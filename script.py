65bc69ffbedbf2d9b47c022b3c98add1b10bd9e1
Ardneliahs <shailendragautam349@gmail.com>
2022-09-07 22:39:52 +0530

    added code

--- script.py
+++ script.py
@@ -1,2 +1,16 @@
+"""
+/var/log/customlog.log
+export to /var/exporter/metrics
+expose this on 0.0.0.0:9800/metrics
+Rest API
+
+"""
+
 if __name__ == "__main__":
-    print("Hello World")
+    f_log = open("/var/log/customlog.log",r)
+    f_metric = open("/var/exporter/metrics",w)
+    f_content = f_log.read()
+    if '400' in f_content:
+        f_metric.write("FourHundredErrors: 1")
+    f_log.close()
+    f_metric.close()
