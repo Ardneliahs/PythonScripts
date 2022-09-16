be0dee69a3d61623b0dd35371360d3433a431dfa
Ardneliahs <shailendragautam349@gmail.com>
2022-09-16 18:22:17 +0530

    finalise logscrape

--- logscrape.py
+++ logscrape.py
@@ -5,6 +5,6 @@ my_string = re.compile(r'.{6}\|[a-zA-Z0-9]{4}\|[0-9]{4}')
 with open("sample.txt") as logfile:
   loglines = logfile.readlines()
   for lines in loglines:
-    my_output = my_string.findall(lines)
-    if len(my_output) != 0:
-      print(lines[0:20] + my_output[0])
+    my_output = my_string.search(lines)
+    if len(my_output.group()) != 0:
+      print(lines[0:20] + my_output.group())
