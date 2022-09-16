f05ebd1409013b6e6741e0b5a6f3439e0df2c966
root <root@localhost.localdomain>
2022-09-16 08:38:33 -0400

    added logscrape

---
+++ logscrape.py
@@ -0,0 +1,10 @@
+import re
+
+my_string = re.compile(r'.{6}\|[a-zA-Z0-9]{4}\|[0-9]{4}')
+
+with open("sample.txt") as logfile:
+  loglines = logfile.readlines()
+  for lines in loglines:
+    my_output = my_string.findall(lines)
+    if len(my_output) != 0:
+      print(lines.rstrip('\n'))
