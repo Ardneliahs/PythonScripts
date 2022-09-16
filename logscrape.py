d43d77649640bfd8701894743bfbfc3ec999d720
Ardneliahs <shailendragautam349@gmail.com>
2022-09-16 18:15:01 +0530

    z

--- logscrape.py
+++ logscrape.py
@@ -7,4 +7,4 @@ with open("sample.txt") as logfile:
   for lines in loglines:
     my_output = my_string.findall(lines)
     if len(my_output) != 0:
-      print(lines[0:19] + my_output[0])
+      print(lines[0:20] + my_output[0])
