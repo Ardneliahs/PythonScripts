3986bf8ab629e333ed96ab7c73bd9ea3daf3a2aa
Ardneliahs <shailendragautam349@gmail.com>
2022-09-16 18:23:34 +0530

    added search in place of findall

--- logscrape.py
+++ logscrape.py
@@ -5,6 +5,6 @@ my_string = re.compile(r'.{6}\|[a-zA-Z0-9]{4}\|[0-9]{4}')
 with open("sample.txt") as logfile:
   loglines = logfile.readlines()
   for lines in loglines:
-    my_output = my_string.search(lines)
+    my_output = re.search(my_string,lines)
     if len(my_output.group()) != 0:
       print(lines[0:20] + my_output.group())
